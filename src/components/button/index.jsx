724eaa07a8790d78a0b3a55396c640b3e563d7b1
Paul <p.rossiter83@gmail.noreply.github.com>
2024-05-10 15:00:15 +0100

    [Feature] button component create and styled

---
+++ src/components/button/index.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+import "./index.scss";
+
+const Button = (props) => {
+    const buttonStyle = {
+        background: props.background,
+        color: props.color,
+        fontSize: props.fontSize + 'px',
+    };
+
+    return ( 
+        <Link
+            className="flat-btn"
+            to={props.link}
+            type={props.type}
+            target={props.target}
+        >
+            <button style={buttonStyle}>{props.text}</button>
+        </Link>
+     );
+}
+ 
+export default Button;
